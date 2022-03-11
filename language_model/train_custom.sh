b14025d6d477a67dc68c9c8c4526f8035518998f
Gonzalo Benegas <gbenegas@berkeley.edu>
2022-03-11 11:52:43 -0800

    Add some missing arguments

--- language_model/train_custom.sh
+++ language_model/train_custom.sh
@@ -9,27 +9,27 @@ python run_mlm_custom.py \
     --save_strategy steps \
     --save_steps 2000 \
     --max_steps 20000 \
+    --evaluation_strategy steps \
     --eval_steps 1000 \
     --seed 42 \
     --fp16 \
     --dataloader_num_workers 8 \
-    --warmup_ratio 0.01 \
+    --preprocessing_num_workers 12 \
+    --warmup_steps 1000 \
+    --logging_steps 1000 \
     --save_total_limit 10 \
     --output_dir results \
-    --config_overrides vocab_size=4096 \
-    --max_seq_length 188 \
-    --per_device_train_batch_size 100 \
-    --gradient_accumulation_steps 10 \
-    --tokenizer_name ./tokenizer_unigram_4091_v3/ \
-#
-#    --output_dir results \
-#    --config_overrides vocab_size=1024,hidden_size=512,num_hidden_layers=8,num_attention_heads=8,intermediate_size=2048 \
-#    --per_device_train_batch_size 120 \
-#    --per_device_eval_batch_size 120 \
-#    --gradient_accumulation_steps 8 \
-#    --tokenizer_name ./tokenizer_unigram_1019_v2/ \
-#    --max_seq_length 220 \
-#
-#
-#
-#    --resume_from_checkpoint ./results/checkpoint-7848
+    --tokenizer_name ./tokenizer_unigram_251_v2/ \
+    --config_overrides vocab_size=256 \
+    --max_seq_length 280 \
+    --per_device_train_batch_size 64 \
+    --per_device_eval_batch_size 64 \
+    --gradient_accumulation_steps 16 \
+
+
+#    --tokenizer_name ./tokenizer_unigram_4091_v3/ \
+#    --config_overrides vocab_size=4096 \
+#    --max_seq_length 188 \
+#    --per_device_train_batch_size 100 \
+#    --per_device_eval_batch_size 100 \
+#    --gradient_accumulation_steps 10 \
