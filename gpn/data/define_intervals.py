625ce308cf558a8ae19d94592d387e0ccb03392f
Gonzalo Benegas <gbenegas@berkeley.edu>
2022-09-27 18:05:59 -0700

    Mask filtering seems to be working

--- gpn/data/define_intervals.py
+++ gpn/data/define_intervals.py
@@ -15,18 +15,43 @@ MASKED_SYMBOLS = list("acgt")
 
 
 def filter_undefined(intervals, genome, min_contig_len):
+    print("filter_undefined")
     def find_defined(interval):
         seq = np.array(list(str(genome[interval.chrom][interval.start:interval.end].seq)))
-        intervals = interval.to_frame().T  # not sure this will work
+        intervals = interval.to_frame().T
         intervals = bf.sanitize_bedframe(intervals)
-        undefined = pd.DataFrame(dict(start=np.where(~np.isin(seq, DEFINED_SYMBOLS))[0]))
+        undefined = pd.DataFrame(dict(start=interval.start + np.where(~np.isin(seq, DEFINED_SYMBOLS))[0]))
         if len(undefined) > 0:
             undefined["chrom"] = interval.chrom
             undefined["end"] = undefined.start + 1
             undefined = bf.merge(undefined)
             intervals = bf.subtract(intervals, undefined)
-        return intervals  # TODO: test this in the else condition
+        intervals = intervals[intervals.end-intervals.start>=min_contig_len]
+        return intervals
     intervals = pd.concat(intervals.progress_apply(find_defined, axis=1).values, ignore_index=True)
+    return intervals
+
+
+def filter_masked(intervals, genome, min_contig_len, mask_incl_context):
+    print("filter_masked")
+    def find_unmasked(interval):
+        seq = np.array(list(str(genome[interval.chrom][interval.start:interval.end].seq)))
+        intervals = interval.to_frame().T
+        intervals = bf.sanitize_bedframe(intervals)
+        masked = pd.DataFrame(dict(start=interval.start + np.where(np.isin(seq, MASKED_SYMBOLS))[0]))
+        if len(masked > 0):
+            masked["chrom"] = interval.chrom
+            masked["end"] = masked.start + 1
+            masked = bf.merge(masked)
+            masked.start += mask_incl_context
+            masked.end -= mask_incl_context
+            masked = masked.query('end-start > 0')
+            if len(masked) > 0:
+                intervals = bf.subtract(intervals, masked)
+        intervals = intervals[intervals.end-intervals.start>=min_contig_len]
+        return intervals
+
+    intervals = pd.concat(intervals.progress_apply(find_unmasked, axis=1).values, ignore_index=True)
     return intervals
 
 
@@ -50,9 +75,10 @@ def main(args):
     if args.filter_undefined:
         intervals = filter_undefined(intervals, genome, args.min_contig_len)
         print(intervals)
-        raise Exception("debug")
     if args.filter_masked:
         intervals = filter_masked(intervals, genome, args.min_contig_len, args.mask_incl_context)
+        print(intervals)
+        raise Exception("debug")
     if args.filter_feature is not None:
         gtf = 0 #pass
         intervals = filter_feature(intervals, gtf, args.filter_feature, args.min_contig_len)
@@ -72,6 +98,7 @@ if __name__ == "__main__":
     )
     parser.add_argument("--output-path", help="Output path", type=str)
     parser.add_argument("--min-contig-len", help="Minimum contig length", type=int)
+    parser.add_argument("--mask-incl-context", help="Border of masked regions included as context", type=int)
     parser.add_argument(
         "--filter-undefined",
         help="Exclude undefined nucleotides (e.g. N)",
