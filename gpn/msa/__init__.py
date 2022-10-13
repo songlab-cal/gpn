9a4c93ee959270d0c0ca9826094cb88b78bc5ccc
Gonzalo Benegas <gbenegas@berkeley.edu>
2022-10-13 16:59:24 -0700

    Remove other code

--- gpn/msa/__init__.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoConfig, AutoModel, AutoModelForMaskedLM
-
-from .model import MSAConvNetConfig, MSAConvNetModel, MSAConvNetForMaskedLM
-
-AutoConfig.register("MSAConvNet", MSAConvNetConfig)
-AutoModel.register(MSAConvNetConfig, MSAConvNetModel)
-AutoModelForMaskedLM.register(MSAConvNetConfig, MSAConvNetForMaskedLM)
