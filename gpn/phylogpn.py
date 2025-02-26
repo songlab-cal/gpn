12e2b3fd943fc248502b218134b4b27bd687d81a
Carlos <calbors@berkeley.edu>
2025-02-25 16:20:02 -0800

    Add wrapper to PhyloGPN HuggingFace repo and example

---
+++ gpn/phylogpn.py
@@ -0,0 +1,4 @@
+from transformers import AutoModel, AutoTokenizer
+checkpoint = "songlab/PhyloGPN"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint, trust_remote_code=True)
+model = AutoModel.from_pretrained(checkpoint, trust_remote_code=True)
